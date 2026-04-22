25256b91e0d09f28fd3990b3eaf3713e4180517d
Denis Lindner <lindnerdenis19@gmail.com>
2026-04-22 12:36:45 -0300

    fix: some deploy configurations updated

--- src/main.ts
+++ src/main.ts
@@ -14,9 +14,11 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  app.enableCors();
+  app.enableCors({
+    origin: [process.env.ORIGIN],
+  });
   app.use(helmet());
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
